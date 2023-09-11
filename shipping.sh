9a28c36b9a465811ea0e3002cbbc17e8d175e428
Haseeb <Mohammad9haseeb@gmail.com>
2023-09-11 00:05:27 -0400

    shipping

--- shipping.sh
+++ shipping.sh
@@ -29,13 +29,16 @@ mv target/${component}-1.0.jar ${component}.jar
 print_head "create service file"
 cp ${script_path}/${component}.service /etc/systemd/system/${component}.service
 
+print_head "load service"
+systemctl daemon-reload
 
 print_head "load schema"
 dnf install mysql -y
+
+print_head "Change MySQl default password"
 mysql -h mysql-dev.haseebdevops.online -uroot -p${mysql_root_pass} < /app/schema/${component}.sql
 
 print_head "start ${component}"
-systemctl daemon-reload
 systemctl enable ${component}
 systemctl start ${component}
 systemctl restart ${component}
