422b505d167edc6baa41e95ed3b7335e3b09f8fd
mohammad9haseeb <mohammad9haseeb@gmail.com>
2023-09-21 16:42:39 -0400

    final code dry 7

--- redis.sh
+++ redis.sh
@@ -2,21 +2,23 @@ script=$(realpath "$0")
 script_path=$(dirname "$script")
 source ${script_path}/common.sh
 
-print_head "download redis & enable 6.2 version"
+component=redis
+
+func_print_head "download redis & enable 6.2 version"
 dnf install https://rpms.remirepo.net/enterprise/remi-release-8.rpm -y &>>$log_file
 func_stat_check $?
 dnf module enable redis:remi-6.2 -y &>>$log_file
 func_stat_check $?
 
-print_head "install redis"
+func_print_head "install redis"
 dnf install redis -y &>>$log_file
 func_stat_check $?
 
-print_head "change listen port"
+func_print_head "change listen port"
 sed -i -e "s|127.0.0.1|0.0.0.0|" /etc/redis.conf /etc/redis/redis.conf &>>$log_file
 func_stat_check $?
 
-print_head "start redis"
+func_print_head "start redis"
 systemctl enable redis &>>$log_file
 func_stat_check $?
 systemctl restart redis &>>$log_file
