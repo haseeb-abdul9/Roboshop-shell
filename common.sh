13bd5e53c8c74a1bd00be1e64ffc93de8ecc79f9
mohammad9haseeb <mohammad9haseeb@gmail.com>
2023-09-21 15:43:53 -0400

    final code dry

--- common.sh
+++ common.sh
@@ -1,63 +1,95 @@
 app_user=roboshop
 mysql_root_pass=$1
 rabbitmq_pass=$1
+log_file=/tmp/roboshop.log
 
 func_print_head() {
   echo -e "\e[35m>>>>>>>> $1 <<<<<<<<\e[0m"
+  echo -e "\e[35m>>>>>>>> $1 <<<<<<<<\e[0m" &>>${log_file}
+}
+func_stat_check() {
+  if [ $1 -eq 0 ]; then
+    echo -e "\e32mSuccess\e[0m"
+  else
+    echo - e "\e31mFailure\e[0m"
+    echo -e "Refer /tmp/roboshop.log for more information"
+    exit 1
+  fi
 }
 
 func_schema_setup() {
   if [ "$schema_setup" == "mongo" ]; then
     func_print_head "create mongo repo"
-    cp ${script_path}/mongo.repo /etc/yum.repos.d/mongo.repo
+    cp ${script_path}/mongo.repo /etc/yum.repos.d/mongo.repo &>>${log_file}
+    func_stat_check $?
 
     func_print_head "load schema"
-    dnf install mongodb-org-shell -y
-    mongo --host mongodb-dev.haseebdevops.online </app/schema/${component}.js
+    dnf install mongodb-org-shell -y &>>${log_file}
+    func_stat_check $?
+    mongo --host mongodb-dev.haseebdevops.online </app/schema/${component}.js &>>${log_file}
+    func_stat_check $?
   fi
   if [ "$schema_setup" == "mysql"]; then
     func_print_head "load schema"
-      dnf install mysql -y
+    dnf install mysql -y &>>${log_file}
+    func_stat_check $?
 
-      func_print_head "Change MySQl default password"
-      mysql -h mysql-dev.haseebdevops.online -uroot -p${mysql_root_pass} < /app/schema/shipping.sql
+    func_print_head "Change MySQl default password"
+    mysql -h mysql-dev.haseebdevops.online -uroot -p${mysql_root_pass} < /app/schema/shipping.sql &>>${log_file}
+    func_stat_check $?
   fi
 }
 
 
 func_app_prereq() {
   func_print_head "add app user & directory"
-    useradd ${app_user}
-    rm -rf /app
-    mkdir /app
+    id ${app_user} &>>${log_file}
+    if [ $? -ne 0 ]; then
+      useradd ${app_user} &>>${log_file}
+    fi
+    func_stat_check $?
+    rm -rf /app &>>${log_file}
+    func_stat_check $?
+    mkdir /app &>>${log_file}
+    func_stat_check $?
 
     func_print_head "download & unzip app content"
-    curl -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip
-    cd /app
-    unzip /tmp/${component}.zip
+    curl -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip &>>${log_file}
+    func_stat_check $?
+    cd /app &>>${log_file}
+    func_stat_check $?
+    unzip /tmp/${component}.zip &>>${log_file}
+    func_stat_check $?
 }
 
 func_systemd_setup() {
    func_print_head "create ${component} service file"
-    cp ${script_path}/${component}.service /etc/systemd/system/${component}.service
+    cp ${script_path}/${component}.service /etc/systemd/system/${component}.service &>>${log_file}
+    func_stat_check $?
 
     func_print_head "start ${component}"
-    systemctl daemon-reload
-    systemctl enable ${component}
-    systemctl restart ${component}
+    systemctl daemon-reload &>>${log_file}
+    func_stat_check $?
+    systemctl enable ${component} &>>${log_file}
+    func_stat_check $?
+    systemctl restart ${component} &>>${log_file}
+    func_stat_check $?
 }
 
 func_nodejs() {
   func_print_head "download nodejs"
-  curl -sL https://rpm.nodesource.com/setup_lts.x | bash
+  curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>${log_file}
+  func_stat_check $?
 
   func_print_head "install nodejs"
-  dnf install nodejs -y
+  dnf install nodejs -y &>>${log_file}
+  func_stat_check $?
 
   func_app_prereq
 
   func_print_head "install NPM"
-  npm install
+  npm install &>>${log_file}
+  func_stat_check $?
 
   func_systemd_setup
   func_schema_setup
@@ -65,25 +97,30 @@ func_nodejs() {
 
 func_python() {
   func_print_head "install python"
-  dnf install python36 gcc python3-devel -y
+  dnf install python36 gcc python3-devel -y &>>${log_file}
+  func_stat_check $?
 
   func_app_prereq
 
   func_print_head "download dependencies"
-  pip3.6 install -r requirements.txt
+  pip3.6 install -r requirements.txt &>>${log_file}
+  func_stat_check $?
 
   func_systemd_setup
 }
 
 func_java() {
   func_print_head "install java"
-  dnf install maven -y
+  dnf install maven -y &>>${log_file}
+  func_stat_check $?
 
   func_app_prereq
 
   func_print_head "download dependencies"
-  mvn clean package
-  mv target/shipping-1.0.jar shipping.jar
+  mvn clean package &>>${log_file}
+  func_stat_check $?
+  mv target/shipping-1.0.jar shipping.jar &>>${log_file}
+  func_stat_check $?
 
   func_schema_setup
   func_systemd_setup
