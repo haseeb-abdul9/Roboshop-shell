eb455e9029624b74a167819311323c7d978988e9
mohammad9haseeb <mohammad9haseeb@gmail.com>
2023-09-21 16:31:23 -0400

    final code dry 5

--- frontend.sh
+++ frontend.sh
@@ -6,20 +6,28 @@ component=frontend
 
 func_print_head "install nginx"
 dnf install nginx -y &>>$log_file
+func_stat_check $?
 
 func_print_head "remove nginx content"
 rm -rf /usr/share/nginx/html/* &>>$log_file
+func_stat_check $?
 
 func_print_head "frontend content"
 curl -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip &>>$log_file
+func_stat_check $?
 
 func_print_head "unzip frontend content"
 cd /usr/share/nginx/html &>>$log_file
+func_stat_check $?
 unzip /tmp/${component}.zip &>>$log_file
+func_stat_check $?
 
 func_print_head "configure nginx reverse proxy"
 cp ${script_path}/roboshop.conf /etc/nginx/default.d/roboshop.conf &>>$log_file
+func_stat_check $?
 
 func_print_head "start nginx"
 systemctl enable nginx &>>$log_file
+func_stat_check $?
 systemctl restart nginx &>>$log_file
+func_stat_check $?
